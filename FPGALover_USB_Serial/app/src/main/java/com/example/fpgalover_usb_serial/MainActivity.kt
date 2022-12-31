4184e7b45e5472af1cb3ca99604b399c208a9e56
anderson.msilva <anderson.msilva@terceiroslinx.com.br>
2022-12-30 21:29:43 -0300

    Projetos de testes

---
+++ FPGALover_USB_Serial/app/src/main/java/com/example/fpgalover_usb_serial/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.fpgalover_usb_serial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
